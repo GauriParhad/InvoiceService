2671e3947ff86b05f1dffed3f28407783881b44b
Gaurip10 <thegauriparhad@gmail.com>
2021-09-26 09:50:29 +0530

    Added: Multiple Rides

--- src/test/java/com/bridgelabz/InvoiceGenerator.java
+++ src/test/java/com/bridgelabz/InvoiceGenerator.java
@@ -3,8 +3,23 @@ package com.bridgelabz;
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM = 10;
     private static final double COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance* MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        double TotalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        if (TotalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return TotalFare;
+    }
+
+    public double calculatetotalfare(Ride[] rides) {
+
+            double totalFare = 0;
+            for (Ride ride : rides) {
+                totalFare += this.calculatetotalfare(ride.distance, ride.time);
+            }
+            return totalFare;
+        }
     }
 }
+
